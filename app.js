e2d5287b84a5c50cab3f33673a6aae76980db35b
orlandogordon <ogordon1998@gmail.com>
2022-05-10 23:10:37 -0400

    Enabled localstorage of user accounts and ability to create new accounts from the home page

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { accounts } from './script.js';
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -8,49 +10,13 @@
 // Data
 
 // DIFFERENT DATA! Contains movement dates, currency and locale
-
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-
-  movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2022-03-14T17:01:17.194Z',
-    '2022-03-18T23:36:17.929Z',
-    '2022-03-19T10:51:36.790Z',
-  ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+window.onload = function () {
+  const storage = localStorage.getItem('accounts');
+  if (storage) {
+    const accounts = JSON.parse(storage);
+  }
+  console.log(accounts);
 };
-
-const account2 = {
-  owner: 'Orlando Gordon',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 1998,
-
-  movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2022-03-14T18:49:59.371Z',
-    '2022-03-18T12:01:20.894Z',
-  ],
-  currency: 'USD',
-  locale: 'en-US',
-};
-
-const accounts = [account1, account2];
-
 /////////////////////////////////////////////////
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -220,104 +186,84 @@ let currentAccount, timer;
 const now = new Date();
 
 // month/day/year
+if (btnLogin) {
+  btnLogin.addEventListener('click', function (e) {
+    // Prevent form from submitting
+    e.preventDefault();
 
-btnLogin.addEventListener('click', function (e) {
-  // Prevent form from submitting
-  e.preventDefault();
+    currentAccount = accounts.find(
+      acc => acc.username === inputLoginUsername.value
+    );
+    console.log(currentAccount);
 
-  currentAccount = accounts.find(
-    acc => acc.username === inputLoginUsername.value
-  );
-  console.log(currentAccount);
+    if (currentAccount?.pin === Number(inputLoginPin.value)) {
+      // Display UI and message
+      labelWelcome.textContent = `Welcome back, ${
+        currentAccount.owner.split(' ')[0]
+      }`;
+      containerApp.style.opacity = 100;
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // Display UI and message
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(' ')[0]
-    }`;
-    containerApp.style.opacity = 100;
+      // Create current date and time
+      const now = new Date();
+      const options = {
+        hour: 'numeric',
+        minute: 'numeric',
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        // weekday: 'long',
+      };
+      // const locale = navigator.language;
+      // console.log(locale);
 
-    // Create current date and time
-    const now = new Date();
-    const options = {
-      hour: 'numeric',
-      minute: 'numeric',
-      day: 'numeric',
-      month: 'numeric',
-      year: 'numeric',
-      // weekday: 'long',
-    };
-    // const locale = navigator.language;
-    // console.log(locale);
+      labelDate.textContent = new Intl.DateTimeFormat(
+        currentAccount.locale,
+        options
+      ).format(now);
 
-    labelDate.textContent = new Intl.DateTimeFormat(
-      currentAccount.locale,
-      options
-    ).format(now);
+      // const day = `${now.getDate()}`.padStart(2, 0);
+      // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+      // const year = now.getFullYear();
+      // const hour = `${now.getHours()}`.padStart(2, 0);
+      // const min = `${now.getMinutes()}`.padStart(2, 0);
+      // labelDate.textContent = `${month}/${day}/${year}, ${hour}:${min}`;
 
-    // const day = `${now.getDate()}`.padStart(2, 0);
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, 0);
-    // const min = `${now.getMinutes()}`.padStart(2, 0);
-    // labelDate.textContent = `${month}/${day}/${year}, ${hour}:${min}`;
+      // Clear input fields
+      inputLoginUsername.value = inputLoginPin.value = '';
+      inputLoginPin.blur();
 
-    // Clear input fields
-    inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur();
+      // Start log out timer
+      if (timer) clearInterval(timer);
+      timer = startLogOutTimer();
 
-    // Start log out timer
-    if (timer) clearInterval(timer);
-    timer = startLogOutTimer();
+      // Update UI
+      updateUI(currentAccount);
+    } else console.log('wrong pin?');
+  });
+}
 
-    // Update UI
-    updateUI(currentAccount);
-  }
-});
+if (btnTransfer) {
+  btnTransfer.addEventListener('click', function (e) {
+    e.preventDefault();
+    const amount = Number(inputTransferAmount.value);
+    const receiverAcc = accounts.find(
+      acc => acc.username === inputTransferTo.value
+    );
+    inputTransferAmount.value = inputTransferTo.value = '';
 
-btnTransfer.addEventListener('click', function (e) {
-  e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
-  const receiverAcc = accounts.find(
-    acc => acc.username === inputTransferTo.value
-  );
-  inputTransferAmount.value = inputTransferTo.value = '';
+    if (
+      amount > 0 &&
+      receiverAcc &&
+      currentAccount.balance >= amount &&
+      receiverAcc?.username !== currentAccount.username
+    ) {
+      // Doing the transfer
+      currentAccount.movements.push(-amount);
+      receiverAcc.movements.push(amount);
 
-  if (
-    amount > 0 &&
-    receiverAcc &&
-    currentAccount.balance >= amount &&
-    receiverAcc?.username !== currentAccount.username
-  ) {
-    // Doing the transfer
-    currentAccount.movements.push(-amount);
-    receiverAcc.movements.push(amount);
-
-    // Add transfer date
-    currentAccount.movementsDates.push(new Date().toISOString());
-    receiverAcc.movementsDates.push(new Date().toISOString());
-
-    // Update UI
-    updateUI(currentAccount);
-
-    // Reset Log out timer
-    clearInterval(timer);
-    timer = startLogOutTimer();
-  }
-});
-
-btnLoan.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  const amount = Math.floor(inputLoanAmount.value);
-
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    setTimeout(function () {
-      currentAccount.movements.push(amount);
-
-      // Add loan date
+      // Add transfer date
       currentAccount.movementsDates.push(new Date().toISOString());
+      receiverAcc.movementsDates.push(new Date().toISOString());
 
       // Update UI
       updateUI(currentAccount);
@@ -325,37 +271,69 @@ btnLoan.addEventListener('click', function (e) {
       // Reset Log out timer
       clearInterval(timer);
       timer = startLogOutTimer();
-    }, 1800);
-  }
-  inputLoanAmount.value = '';
-});
+    }
+  });
+}
 
-btnClose.addEventListener('click', function (e) {
-  e.preventDefault();
+if (btnLoan) {
+  btnLoan.addEventListener('click', function (e) {
+    e.preventDefault();
 
-  if (
-    inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
-  ) {
-    const index = accounts.findIndex(
-      acc => acc.username === currentAccount.username
-    );
-    console.log(index);
-    // .indexOf(23)
+    const amount = Math.floor(inputLoanAmount.value);
 
-    // Delete account
-    accounts.splice(index, 1);
+    if (
+      amount > 0 &&
+      currentAccount.movements.some(mov => mov >= amount * 0.1)
+    ) {
+      // Add movement
+      setTimeout(function () {
+        currentAccount.movements.push(amount);
 
-    // Hide UI
-    containerApp.style.opacity = 0;
-  }
+        // Add loan date
+        currentAccount.movementsDates.push(new Date().toISOString());
 
-  inputCloseUsername.value = inputClosePin.value = '';
-});
+        // Update UI
+        updateUI(currentAccount);
+
+        // Reset Log out timer
+        clearInterval(timer);
+        timer = startLogOutTimer();
+      }, 1800);
+    }
+    inputLoanAmount.value = '';
+  });
+}
+
+if (btnClose) {
+  btnClose.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (
+      inputCloseUsername.value === currentAccount.username &&
+      Number(inputClosePin.value) === currentAccount.pin
+    ) {
+      const index = accounts.findIndex(
+        acc => acc.username === currentAccount.username
+      );
+      console.log(index);
+      // .indexOf(23)
+
+      // Delete account
+      accounts.splice(index, 1);
+
+      // Hide UI
+      containerApp.style.opacity = 0;
+    }
+
+    inputCloseUsername.value = inputClosePin.value = '';
+  });
+}
 
 let sorted = false;
-btnSort.addEventListener('click', function (e) {
-  e.preventDefault();
-  displayMovements(currentAccount, !sorted);
-  sorted = !sorted;
-});
+if (btnSort) {
+  btnSort.addEventListener('click', function (e) {
+    e.preventDefault();
+    displayMovements(currentAccount, !sorted);
+    sorted = !sorted;
+  });
+}
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-'use strict';
+('use strict');
+
+// import { accounts } from './app.js';
 
 // Todo - Tie in the actual baking app. find a way to transfer the data between scrits. potentially refractor into more js files. Add hints/visual pop ups to improve ux. Push to git.
 
@@ -16,269 +18,349 @@ const form = document.querySelector('.modal__form');
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
 const pin = document.querySelector('#pin');
+export let accounts = [];
+
+////////////////////////////////////////
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2022-03-14T17:01:17.194Z',
+    '2022-03-18T23:36:17.929Z',
+    '2022-03-19T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Orlando Gordon',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 1998,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2022-03-14T18:49:59.371Z',
+    '2022-03-18T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const defaultAccounts = [account1, account2];
+const init = function () {
+  const storage = localStorage.getItem('accounts');
+  if (storage) {
+    accounts = JSON.parse(storage);
+  } else {
+    localStorage.setItem('accounts', JSON.stringify(defaultAccounts));
+    console.log('setting up the accounts in storage');
+    init();
+  }
+};
+init();
+
+console.log(accounts);
+///////////////////////////////////////
+
 ///////////////////////////////////////
 // Modal window
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
-
-btnScrollTo.addEventListener('click', function (e) {
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-////////////////////////////////////////////////////////
-// Page Navigation
-
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({
-//       behavior: 'smooth',
-//     });
-//   });
-// });
-
-// Event Delegation
-// 1. Add Event listener to common parent element
-// 2. Determin what element originated the event
-
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  e.preventDefault();
-
-  // Matching strategy
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({
-      behavior: 'smooth',
-    });
-  }
-});
-////////////////////////////////////////////////////////
-
-// Tabbed Component
-// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
-
-tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
-
-  if (!clicked) return; // Guard clause
-
-  // Remove active classes
-  tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-
-  // Activate tab
-  clicked.classList.add('operations__tab--active');
-
-  // Active Content Area
-  document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add('operations__content--active');
-});
-
-// Menu Fade Animation
-const handleHover = function (e) {
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
-
-    siblings.forEach(el => {
-      if (el !== link) el.style.opacity = this;
-    });
-    logo.style.opacity = this;
-  }
-};
-
-// Passing "argument" into handler using the bind method
-nav.addEventListener('mouseover', handleHover.bind(0.5));
-
-nav.addEventListener('mouseout', handleHover.bind(1));
-
-// Sticky Navigation
-const header = document.querySelector('.header');
-const navHeight = nav.getBoundingClientRect().height;
-
-const stickyNav = function (entries) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
-};
-
-const headObserver = new IntersectionObserver(stickyNav, {
-  root: null,
-  threshold: 0,
-  rootMargin: `-${navHeight}px`,
-});
-
-headObserver.observe(header);
-
-// Revealing sections(elements) as we scroll
-const allSections = document.querySelectorAll('.section');
-
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
-
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-});
-
-// Lazy loading images
-const imgTargets = document.querySelectorAll('img[data-src]');
-
-const loadImg = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  // Replace src with data-src
-  entry.target.src = entry.target.dataset.src;
-  entry.target.addEventListener('load', function () {
-    entry.target.classList.remove('lazy-img');
-  });
-  observer.unobserve(entry.target);
-};
-
-const imgObserver = new IntersectionObserver(loadImg, {
-  root: null,
-  threshold: 0,
-  rootMargin: '200px',
-});
-
-imgTargets.forEach(img => imgObserver.observe(img));
-
-// Slider
-const slider = function () {
-  const slides = document.querySelectorAll('.slide');
-  const btnLeft = document.querySelector('.slider__btn--left');
-  const btnRight = document.querySelector('.slider__btn--right');
-  const dotContainer = document.querySelector('.dots');
-
-  let curSlide = 0;
-  const maxSlide = slides.length;
-
-  //Functions
-  const createDots = function () {
-    slides.forEach(function (_, i) {
-      dotContainer.insertAdjacentHTML(
-        'beforeend',
-        `<button class="dots__dot" data-slide="${i}"></button>`
-      );
-    });
+if (modal) {
+  const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   };
 
-  const activateDot = function (slide) {
-    document
-      .querySelectorAll('.dots__dot')
-      .forEach(dot => dot.classList.remove('dots__dot--active'));
-
-    document
-      .querySelector(`.dots__dot[data-slide="${slide}"]`)
-      .classList.add('dots__dot--active');
+  const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
   };
 
-  const goToSlide = function (slide) {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-    );
-  };
+  btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-  // Next Slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const prevSlide = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-    activateDot(curSlide);
-  };
-
-  const init = function () {
-    createDots();
-    goToSlide(0);
-    activateDot(0);
-  };
-  init();
-
-  // Event Handlers
-  btnRight.addEventListener('click', nextSlide);
-  btnLeft.addEventListener('click', prevSlide);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 
   document.addEventListener('keydown', function (e) {
-    if (e.key === 'ArrowLeft') prevSlide();
-    e.key === 'ArrowRight' && nextSlide();
-  });
-
-  dotContainer.addEventListener('click', function (e) {
-    if (e.target.classList.contains('dots__dot')) {
-      const { slide } = e.target.dataset;
-      goToSlide(slide);
-      activateDot(slide);
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
     }
   });
-};
-slider();
 
-// window.addEventListener('beforeunload', function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   e.returnValue = '';
-// });
+  btnScrollTo.addEventListener('click', function (e) {
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
+  ////////////////////////////////////////////////////////
+  // Page Navigation
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  console.log(e);
-  var select1 = firstName.value;
-  var select2 = lastName.value;
-  var select3 = pin.value;
-  console.log(select1, select2, select3);
-  for (var i = 0; i < form.elements.length; i++) {
-    var el = form.elements[i];
-    console.log('el');
-  }
-});
+  // document.querySelectorAll('.nav__link').forEach(function (el) {
+  //   el.addEventListener('click', function (e) {
+  //     e.preventDefault();
+  //     const id = this.getAttribute('href');
+  //     console.log(id);
+  //     document.querySelector(id).scrollIntoView({
+  //       behavior: 'smooth',
+  //     });
+  //   });
+  // });
+
+  // Event Delegation
+  // 1. Add Event listener to common parent element
+  // 2. Determin what element originated the event
+
+  document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');
+      console.log(id);
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
+  ////////////////////////////////////////////////////////
+
+  // Tabbed Component
+  // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+  tabsContainer.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.operations__tab');
+
+    if (!clicked) return; // Guard clause
+
+    // Remove active classes
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+    // Activate tab
+    clicked.classList.add('operations__tab--active');
+
+    // Active Content Area
+    document
+      .querySelector(`.operations__content--${clicked.dataset.tab}`)
+      .classList.add('operations__content--active');
+  });
+
+  // Menu Fade Animation
+  const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+      const link = e.target;
+      const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+      const logo = link.closest('.nav').querySelector('img');
+
+      siblings.forEach(el => {
+        if (el !== link) el.style.opacity = this;
+      });
+      logo.style.opacity = this;
+    }
+  };
+
+  // Passing "argument" into handler using the bind method
+  nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+  nav.addEventListener('mouseout', handleHover.bind(1));
+
+  // Sticky Navigation
+  const header = document.querySelector('.header');
+  const navHeight = nav.getBoundingClientRect().height;
+
+  const stickyNav = function (entries) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+  };
+
+  const headObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+  });
+
+  headObserver.observe(header);
+
+  // Revealing sections(elements) as we scroll
+  const allSections = document.querySelectorAll('.section');
+
+  const revealSection = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  };
+
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+  });
+
+  allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+  });
+
+  // Lazy loading images
+  const imgTargets = document.querySelectorAll('img[data-src]');
+
+  const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener('load', function () {
+      entry.target.classList.remove('lazy-img');
+    });
+    observer.unobserve(entry.target);
+  };
+
+  const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+  });
+
+  imgTargets.forEach(img => imgObserver.observe(img));
+
+  // Slider
+  const slider = function () {
+    const slides = document.querySelectorAll('.slide');
+    const btnLeft = document.querySelector('.slider__btn--left');
+    const btnRight = document.querySelector('.slider__btn--right');
+    const dotContainer = document.querySelector('.dots');
+
+    let curSlide = 0;
+    const maxSlide = slides.length;
+
+    //Functions
+    const createDots = function () {
+      slides.forEach(function (_, i) {
+        dotContainer.insertAdjacentHTML(
+          'beforeend',
+          `<button class="dots__dot" data-slide="${i}"></button>`
+        );
+      });
+    };
+
+    const activateDot = function (slide) {
+      document
+        .querySelectorAll('.dots__dot')
+        .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+      document
+        .querySelector(`.dots__dot[data-slide="${slide}"]`)
+        .classList.add('dots__dot--active');
+    };
+
+    const goToSlide = function (slide) {
+      slides.forEach(
+        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+      );
+    };
+
+    // Next Slide
+    const nextSlide = function () {
+      if (curSlide === maxSlide - 1) {
+        curSlide = 0;
+      } else {
+        curSlide++;
+      }
+
+      goToSlide(curSlide);
+      activateDot(curSlide);
+    };
+
+    const prevSlide = function () {
+      if (curSlide === 0) {
+        curSlide = maxSlide - 1;
+      } else {
+        curSlide--;
+      }
+      goToSlide(curSlide);
+      activateDot(curSlide);
+    };
+
+    const init = function () {
+      createDots();
+      goToSlide(0);
+      activateDot(0);
+    };
+    init();
+
+    // Event Handlers
+    btnRight.addEventListener('click', nextSlide);
+    btnLeft.addEventListener('click', prevSlide);
+
+    document.addEventListener('keydown', function (e) {
+      if (e.key === 'ArrowLeft') prevSlide();
+      e.key === 'ArrowRight' && nextSlide();
+    });
+
+    dotContainer.addEventListener('click', function (e) {
+      if (e.target.classList.contains('dots__dot')) {
+        const { slide } = e.target.dataset;
+        goToSlide(slide);
+        activateDot(slide);
+      }
+    });
+  };
+  slider();
+
+  // window.addEventListener('beforeunload', function (e) {
+  //   e.preventDefault();
+  //   console.log(e);
+  //   e.returnValue = '';
+  // });
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let usernames = [];
+    let username = firstName.value[0] + lastName.value[0];
+    accounts.forEach(el => usernames.push(el.username));
+    console.log(usernames);
+    console.log(username);
+
+    while (usernames.some(el => el == username)) {
+      let i = 2;
+      console.log('in the loop');
+      username = `${username}${i}`;
+      alert(`Your username was taken so it will be: ${username}`);
+      i++;
+    }
+
+    const newAccount = {
+      owner: `${firstName.value} ${lastName.value}`,
+      movements: [10000],
+      interestRate: 1.5,
+      pin: +pin.value,
+      username: username,
+
+      movementsDates: [new Date().toISOString()],
+      currency: 'USD',
+      locale: 'en-US',
+    };
+    accounts.push(newAccount);
+    localStorage.setItem('accounts', JSON.stringify(accounts));
+    window.location.href = 'app.html';
+  });
+}
